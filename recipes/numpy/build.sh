8632c2ae24e7549d02fcb564d62b440462a2f527
Víctor Molina García <molinav@users.noreply.github.com>
2022-01-06 18:39:16 +0100

    Fix problems related to repair job

--- recipes/numpy/build.sh
+++ recipes/numpy/build.sh
@@ -17,9 +17,30 @@ case ${pyversion} in
         exit 1
     ;;
 esac
+
+# Needed by Python 3.2 as long as it uses pip 7.x.
 case ${pyversion} in
     3.2)  download="install --download ." ;;
     *)    download="download" ;;
+esac
+
+# Needed this upgrade because otherwise `wheel` sets wrong tags for containers.
+case ${pyversion} in
+    3.3)  pip install "wheel < 0.31" --upgrade;;
+    2.6)  sed -i '
+        103 d
+        104 a \    result = distutils.util.get_platform().replace(".", "_").replace("-", "_")
+        104 a \    if result.startswith("macosx") and archive_root is not None:
+        104 a \        result = calculate_macosx_platform_tag(archive_root, result)
+        104 a \    if result == "linux_x86_64" and sys.maxsize == 2147483647:
+        104 a \        result = "linux_i686"
+        104 a \    return result
+    ' /opt/pyenv/versions/*/lib/python2.6/site-packages/wheel/pep425tags.py
+          sed -i '
+          6 a \import sys
+        141 a \            if plat_name in ("linux-x86_64", "linux_x86_64") and sys.maxsize == 2147483647:
+        141 a \                plat_name = "linux_i686"
+    ' /opt/pyenv/versions/*/lib/python2.6/site-packages/wheel/bdist_wheel.py
 esac
 
 # NumPy < 1.12 needs `xlocale.h`.
