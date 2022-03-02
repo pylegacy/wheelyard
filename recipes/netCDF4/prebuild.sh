de9947e09d59525c85341815fbe9a768b91c53e9
Víctor Molina García <molinav@users.noreply.github.com>
2022-03-02 19:05:19 +0100

    Fix CA certificates override

--- recipes/netCDF4/prebuild.sh
+++ recipes/netCDF4/prebuild.sh
@@ -2,6 +2,9 @@
 
 apt-get update
 
+# Make temporary backup of SSL certificates.
+cp -R /etc/ssl /tmp/tmp-etc-ssl
+
 # Install library dependencies.
 pkghdf5="$(apt-cache search libhdf5-serial | grep dev | cut -d' ' -f1)"
 pkgnetcdf4="$(apt-cache search libnetcdf-dev | grep dev | cut -d' ' -f1)"
@@ -9,3 +12,7 @@ apt-get install -y ${pkghdf5} ${pkgnetcdf4}
 
 # Install build dependencies.
 apt-get install -y gcc libc6-dev unzip
+
+# Restore SSL certificates.
+rm -rf /etc/ssl
+mv /tmp/tmp-etc-ssl /etc/ssl
